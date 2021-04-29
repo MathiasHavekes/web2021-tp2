06a6e42a4483a0c7b5836bf40d5390890640fb21
Hadjeres Amar <Planteramartin@gmail.com>
2021-04-29 15:26:37 -0400

    ajout du dropdown et du bouton louer pour la page de location

--- src/views/RentCar.vue
+++ src/views/RentCar.vue
@@ -2,16 +2,23 @@
   <div class="rent-car">
     <v-form>
       <DatePicker/>
+      <DropDown style = "position:absolute;top:400px;left:20px"/>
+      <Bouton style = "position:absolute;top:490px;left:30px"/>
+      
     </v-form>
   </div>
 </template>
 
 <script>
-import DatePicker from "../components/DatePicker"
+import DatePicker from "../components/DatePicker";
+import DropDown from "../components/DropDown";
+import Bouton from"../components/Button"
 
 export default {
   components: {
-    DatePicker: DatePicker
+    DatePicker: DatePicker,
+    DropDown: DropDown,
+    Bouton: Bouton
   }
 };
 </script>
